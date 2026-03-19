680c1b0c1ebeec8bd797b6217f5f20cdfcf180dd
Waldo <SidVal@users.noreply.github.com>
2026-03-19 15:54:36 -0300

    Update codigos.data.mjs

--- docs/codigos/codigos.data.mjs
+++ docs/codigos/codigos.data.mjs
@@ -1,13 +1,11 @@
 import { createContentLoader } from 'vitepress'
 
-export default createContentLoader('codigos/*/index.md', { // <--- Cambiado a index.md
-  includeSrc: false,
-  render: false,
+export default createContentLoader('codigos/*/index.md', {
   transform(raw) {
     return raw
       .map(({ url, frontmatter }) => ({
-        // Reemplazamos /index.html por una barra final / para el link
-        url: url.replace('index.html', ''), 
+        // Agregamos /abap y limpiamos el index.html
+        url: `/abap${url.replace('index.html', '')}`, 
         title: frontmatter.title || 'Programa ABAP',
         description: frontmatter.description || 'Documentación técnica.'
       }))
